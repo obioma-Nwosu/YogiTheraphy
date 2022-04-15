8ab85907c94efe885e6f1353b0e45d3a5e6866bf
obi <obioma.nwosu@fau.de>
2022-04-15 17:22:10 +0200

    initial DB setup

--- Backend/app.js
+++ Backend/app.js
@@ -13,19 +13,52 @@ require('dotenv/config')
 const api = process.env.API_URL
 const db = process.env.DB_URL
 
-app.get(`${api}/asanas` , (req, res) => {
-  const asana = {
-    id: 1,
-    name: 'cow pose',
-    image: 'some_image'
+
+//Schemas
+const asanaSchema = mongoose.Schema({
+  isFav: Boolean,
+  name: String,
+  sanskrit: String,
+  level: Array,
+  type: Array,
+  image: String,
+  effect: String,
+  description: String,
+  watchout: String
+})
+
+//models
+const Asana = mongoose.model('Asana', asanaSchema)
+
+app.get(`${api}/asanas` , async (req, res) => {
+  const asanaList = await Asana.find()
+  if(!asanaList){
+    res.status(500).json({success: false})
   }
-  res.send(asana)
+  res.send(asanaList)
 })
 
 app.post(`${api}/asanas` , (req, res) => {
-  const newAsana = req.body
-  console.log(newAsana)
-  res.send(newAsana)
+  const asana = new Asana({
+    isFav: req.body.isFav,
+    name: req.body.name,
+    sanskrit: req.body.sanskrit,
+    level: req.body.level,
+    type: req.body.type,
+    image: req.body.image,
+    effect: req.body.effect,
+    description: req.body.description,
+    watchout: req.body.watchout,
+  })
+
+  asana.save().then((createdAsana => {
+    res.status(201).json(createdAsana)
+  })).catch((err) => {
+    res.status(500).json({
+      error: err,
+      success: false
+    })
+  })
 })
 
 mongoose.connect(`${db}`)
